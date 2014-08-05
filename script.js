021038ee371e3bc075d5a6ecc7688cf6ca758c42
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-08-06 01:03:20 +0200

    Show the results on the map.

--- script.js
+++ script.js
@@ -5,17 +5,64 @@ var geocoder;
 var center = 'REPLACE_ME';
 var pins = {'replace': 'me'};
 
-function centerArea(geocoderResults) {
-  var firstResult = geocoderResults[0];
-  var options = {
-    zoom: 8,
-    center: firstResult.geometry.location,
+var markers = [];
+
+function withPositionFor(address, callback) {
+  geocoder.geocode({ address: address }, function(geocoderResults, error) {
+    if (geocoderResults != null) {
+      var firstResult = geocoderResults[0];
+      var latLng = firstResult.geometry.location;
+      callback(latLng)
+    } else {
+      console.log("not found", address, error);
+    }
+  });
+}
+
+function contentStringFor(location) {
+  var firstLink = pins[location][0];
+  return "<iframe width='1024' height='600' src='" + firstLink +"'></iframe>";
+}
+
+function insertMarkers() {
+  var i = 0;
+  for (var location in pins) {
+    if (pins.hasOwnProperty(location)) {
+      i = i + 1000;
+      (function(location, links, delay) {
+        setTimeout(function() {
+          withPositionFor(location, function(latLng) {
+            // var infowindow = new google.maps.InfoWindow({
+            //   content: contentStringFor(closureLocation),
+            //   maxWidth: 1200,
+            // });
+
+            var marker = new google.maps.Marker({
+              position: latLng,
+              map: map,
+              title: 'Résulat (' + links.length + ') pour : ' + location,
+            });
+
+            google.maps.event.addListener(marker, 'click', function() {
+              //infowindow.open(map,marker);
+              var url = links[0];
+              window.open(url, '_blank');
+            });
+
+            markers.push(marker);
+          });
+        }, delay);
+
+      })(location, pins[location], i)
+    }
   }
-  map = new google.maps.Map(document.getElementById("map-canvas"), options);
 }
 
 $(function(){
   geocoder = new google.maps.Geocoder();
-  geocoder.geocode({ address: center, }, centerArea);
+  withPositionFor(center, function(latLng) {
+    var options = { zoom: 8, center: latLng }
+    map = new google.maps.Map(document.getElementById("map-canvas"), options);
+    insertMarkers();
+  });
 });
-
