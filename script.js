dfa1e478fd949fed71de6655c702edadf77bff0c
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-08-19 07:37:37 +0200

    Add caching for locations.

--- script.js
+++ script.js
@@ -7,16 +7,36 @@ var pins = {'replace': 'me'};
 
 var markers = [];
 
+function cacheStore(address, latLng) {
+  var latLngCode = latLng !== null ? ('' + latLng.lat() + '|' + latLng.lng()) : null;
+  localStorage.setItem('position_' + address, latLngCode);
+}
+
+function cacheLookup(address) {
+  var cache = localStorage.getItem('position_' + address);
+  if (cache === false || cache === null) return cache;
+
+  var latLng = cache.split('|');
+  return new google.maps.LatLng(parseFloat(latLng[0]), parseFloat(latLng[1]));
+}
+
 function withPositionFor(address, callback) {
-  geocoder.geocode({ address: address }, function(geocoderResults, error) {
-    if (geocoderResults != null) {
-      var firstResult = geocoderResults[0];
-      var latLng = firstResult.geometry.location;
-      callback(latLng)
-    } else {
-      console.log("not found", address, error);
-    }
-  });
+  var result = cacheLookup(address);
+  if (result === null) {
+    geocoder.geocode({ address: address }, function(geocoderResults, error) {
+      if (geocoderResults != null) {
+        var firstResult = geocoderResults[0];
+        var latLng = firstResult.geometry.location;
+        cacheStore(address, latLng);
+        callback(latLng)
+      } else {
+        cacheStore(address, false);
+        console.log("not found", address, error);
+      }
+    });
+  } else if (result !== false) {
+    callback(result);
+  }
 }
 
 function contentStringFor(location) {
